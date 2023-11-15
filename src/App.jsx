6f36f8871dd2bc0d930566738b89a2eec6e38e84
Jaylen <jmowens90@gmail.com>
2023-11-15 17:09:25 -0500

    forgot to do first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import ContactList from './ContactList'
+
+
+
+
+function App() {
+
+  return (
+    <>
+    <ContactList />
+    </>
+  )
+}
+
+export default App
